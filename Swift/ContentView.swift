7c638e268398cb2c4a01c66e5e9d2897de101d0a
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-06-01 17:31:25 -0300

    Initial Commit

---
+++ Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift
+//
+//  Created by LILIA MARIANGEL on 06/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftApp.swift
+//  Swift
+//
+//  Created by LILIA MARIANGEL on 06/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
