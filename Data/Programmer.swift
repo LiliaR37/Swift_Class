5c38e4ee96ef24f3d6dc1ebf47b54b3e29c0306f
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-06-09 11:05:23 -0300

    CreacionTabla

---
+++ Data/Programmer.swift
@@ -0,0 +1,17 @@
+//
+//  Programmer.swift
+//  proyecto
+//
+//  Created by LILIA MARIANGEL on 06/08/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Programmer {
+    
+    var id: Int
+    var name: String
+    var languages: String
+    var avatar: Image
+}
